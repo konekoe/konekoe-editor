f0b894def2328285068610767b3d2ddc4efdf680
lari alakukku <lari.alakukku@aalto.fi>
2021-03-03 12:09:18 +0200

    Added types for submissions and exercises

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,37 @@ export interface ErrorState {
   queue: Error[]
 }
 
+export type ExerciseDictionary<T> = { [exerciseId: string]: T };
+
 export interface TerminalState {
-  output: Map<string, string[]>;
-  input: Map<string, string[]>;
+  output: ExerciseDictionary<string[]>;
+  input: ExerciseDictionary<string[]>;
+}
+
+export interface SubmissionRequest {
+  exerciseId: string;
+  files: { [filename: string]: string };
+}
+
+export interface SubmissionResponse {
+  exerciseId: string;
+  points: number;
+  maxPoints: number;  // NOTE: The backend currently has two ways of defining max points: the database and the grader. Only one should be used so that max points don't need to be received here.
+  error?: Error;
+}
+
+export interface Exercise {
+  id: string;
+  points: number;
+  maxPoints: number;
+  submissions: string[]; // Array of submission ids.
+}
+
+export interface SubmissionState {
+  submissions: ExerciseDictionary<string[]>;                              // Exercise ID to array of submission ids.
+  activeSubmissions: ExerciseDictionary<{ [filename: string]: string }>;  // Submissions made by the user that are being processed.
+  points: ExerciseDictionary<number>;                                     // The most points received from a submission per exercise.
+  maxPoints: ExerciseDictionary<number>;                                  // The maximum points that can be received for each exercise.
 }
 
 export interface TerminalMessage {
