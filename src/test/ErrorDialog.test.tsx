836689d41fd661ca3ad9a9809ea9085e6a0daeb8
lari alakukku <lari.alakukku@aalto.fi>
2021-03-11 09:25:21 +0200

    Fixed issue where tests where checking only immediate content of modal

--- src/test/ErrorDialog.test.tsx
+++ src/test/ErrorDialog.test.tsx
@@ -3,24 +3,26 @@ import { render, fireEvent } from "@testing-library/react";
 import ErrorDialog from "../components/ErrorDialog";
 
 describe("<ErrorDialog />", () => {
-  it("describe should display message and title", () => {
+  it("should display message and title", () => {
     const component = render(<ErrorDialog open={ true } title="Error" message="Hello there" closeHandler={ jest.fn() } />);
     
-    expect(component.container).toHaveTextContent("Hello there");
-    expect(component.container).toHaveTextContent("Error");
+    console.log(component.container.innerHTML);
+
+    expect(component.getByText("Hello there")).not.toBeNull();
+    expect(component.getByText("Error")).not.toBeNull();
   });
 
   it("should display the number of remaining errors", () => {
     const component = render(<ErrorDialog open={ true } title="Error" message="Hello there" numOfRemainingErrors={ 5 } closeHandler={ jest.fn() } />);
 
-    expect(component.container).toHaveTextContent("5");
+    expect(component.getByText("5")).not.toBeNull();
   });
 
   it("clicking close should fire the close handler", () => {
     const closeHandler = jest.fn();
     const component = render(<ErrorDialog open={ true } title="Error" message="Hello there" numOfRemainingErrors={ 5 } closeHandler={ closeHandler } />);
 
-    const cancelButton = component.getByText("Cancel");
+    const cancelButton = component.getByText("Close");
 
     fireEvent.click(cancelButton);
 
