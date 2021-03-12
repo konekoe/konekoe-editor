4e4e9d48bdf75f3fccfea5e8307966e811ecbf07
lari alakukku <lari.alakukku@aalto.fi>
2021-03-12 14:22:18 +0200

    Fixed structure of test state

---
+++ src/test/CodeEditor.test.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { render, fireEvent, RenderResult } from "@testing-library/react";
+import CodeEditor from "../components/CodeEditor/";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { submit } from "../state/submissionsSlice";
+
+const exerciseId1 = "some-exercise1";
+const exerciseId2 = "some-exercise2";
+
+const fileId1 = "file1";
+const fileId2 = "file2";
+const fileId3 = "file3";
+
+const TAB_CLASS_NAME = "MuiButtonBase-root";
+
+describe("<CodeEditor />", () => {
+  let store = configureStore()({});
+
+  beforeEach(() => {
+    store = configureStore()({
+      submissions: {
+        allSubmissions: [], // TODO: Add suport for fetching different submissions
+        activeSubmissions: {
+          [exerciseId1]: {
+            [fileId1]: {
+              fileId: fileId1,
+              filename: "main.c",
+              data: "some code here"
+            },
+            [fileId2]: {
+              fileId: fileId2,
+              filename: "main.h",
+              data: "function prototypes here"
+            }
+          },
+          [exerciseId2]: {
+            [fileId3]: {
+              fileId: fileId3,
+              filename: "ansers.txt",
+              data: "your answers here."
+            }
+          }
+        },
+        points: {
+          [exerciseId1]: 0,
+          [exerciseId2]: 10
+        },
+        maxPoints: {
+          [exerciseId1]: 10,
+          [exerciseId2]: 20
+        }
+      }
+    });
+
+    store.dispatch = jest.fn();
+  });
+
+  it("creates correct number of tabs", () => {
+    const component = render(
+      <Provider store={ store }>
+        <CodeEditor exerciseId={ exerciseId1 }/>
+      </Provider>
+    );
+
+    expect(component.container.querySelectorAll(`.${ TAB_CLASS_NAME }`)).toHaveLength(2);
+
+    component.rerender(
+      <Provider store={ store }>
+        <CodeEditor exerciseId={ exerciseId2 }/>
+      </Provider>
+    );
+    expect(component.container.querySelectorAll(`.${ TAB_CLASS_NAME }`)).toHaveLength(1);
+  });
+
+  it("ace editor is attached", () => {
+    const component = render(
+      <Provider store={ store }>
+        <CodeEditor exerciseId={ exerciseId1 }/>
+      </Provider>
+    );
+
+    expect(component.container.querySelector(".ace_editor")).not.toBeNull();
+  });
+
+  it("editor sessions can be changed by clicking on tabs", () => {
+    expect(true).toBeFalsy();
+  });
+
+  describe("code can be submitted", () => {
+    it("clicking the submission button sends a code submission action", () => {
+      expect(true).toBeFalsy();
+    });
+
+    it("when a code submission is being processed, a message overlay is shown", () => {
+      expect(true).toBeFalsy();
+    });
+
+    it("when a code submission is being processed, another submission can't be sent", () => {
+      expect(true).toBeFalsy();
+    });
+  });
+});
