872d965af22680749c77d6b80424dee566fdb980
lari alakukku <lari.alakukku@aalto.fi>
2021-03-24 10:57:01 +0200

    Added tests for rendering the editor with manually set state

--- cypress/integration/editor_page_setup_spec..ts
+++ cypress/integration/editor_page_setup_spec..ts
@@ -1,3 +1,6 @@
+import { init } from "../../src/state/exerciseSlice";
+import { setActiveSubission } from "../../src/state/submissionsSlice";
+
 describe("Opening the editor page", function () {
   beforeEach(function() {
     cy.visit("/");
@@ -5,5 +8,50 @@ describe("Opening the editor page", function () {
 
   it("Displays a wait screen", function () {
     cy.contains("Just a moment");
+  });
+
+  describe("displays exercise data after init", function () {
+    beforeEach(function() {
+      cy.window()
+      .its("store")
+      .invoke("dispatch", init([
+        { id: "ex1", submissions: [], title: "Some exercise", points: 1, maxPoints: 1, description: "Do something" },
+        { id: "ex2", submissions: [], title: "Some exercise", points: 0, maxPoints: 10, description: "# Works like a charm" }
+      ]));  
+    });
+    
+    it("tabs and exercise description are shown", function (){
+      cy.contains("1/1 | Some exercise");
+      cy.contains("Do something");
+    });
+
+    it("without submission data, a default code file is shown", function() {
+      cy.contains("No files received");
+    });
+
+    it("code editor is updated after an active sumbission has been set", function() {
+      cy.window()
+      .its("store")
+      .invoke("dispatch", setActiveSubission({ 
+        exerciseId: "ex1", 
+        data: [
+          {
+            fileId: "file1",
+            filename: "test.js",
+            data: "console.log('Hello');"
+          },
+          {
+            fileId: "file2",
+            filename: "text.txt",
+            data: "Just some text"
+          }
+        ] 
+      }));
+
+      cy.contains("test.js");
+      cy.contains("text.txt");
+      cy.contains("console.log('Hello');");      
+    });
+
   });
 });
