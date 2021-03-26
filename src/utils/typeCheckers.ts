98fdf0733a8b867ee91cc8d33ecd249c439b5bf3
lari alakukku <lari.alakukku@aalto.fi>
2021-03-26 11:38:52 +0200

    Added type checkers

--- src/utils/typeCheckers.ts
+++ src/utils/typeCheckers.ts
@@ -1,15 +1,41 @@
-import { PointsProp } from "../types";
+import { PointsProp, Exercise } from "../types";
+
+const multiCheck = (searchValues: string[], typeCheckers: Array<(p: unknown) => boolean>, target: Record<string, unknown>): boolean => {
+  return searchValues.reduce((acc: boolean, curr: string, index: number) => acc && typeCheckers[index](target[curr]), true);
+}; 
 
 export const isString = (param: unknown): param is string => typeof param === "string" || param instanceof(String);
 
+export const isStringArray = (param: unknown): param is string[] => {
+  if (!(param instanceof Array))
+    return false;
+
+  return param.reduce((acc: boolean, curr: unknown) => acc && isString(curr), true);
+};
+
 export const isNumber = (param: unknown): param is number => !isNaN(Number(param));
 
-export const isPointsProp = (param: unknown): param is PointsProp => {
+export const isStringRecord = (param: unknown): param is Record<string, unknown> => {
   if (typeof param !== "object" || param === null)
     return false;
-  
-  const multiInclude = (searchValues: string[], target: string[]): boolean => searchValues.reduce((acc: boolean, curr: string) => acc && target.includes(curr), true); 
-  
-  return multiInclude(["receivedPoints", "maxPoints"], Object.keys(param))
-    && Object.values(param).reduce((acc: boolean, curr: unknown) => acc && isNumber(curr), true);
+
+  return isStringArray(Object.keys(param));
+};
+
+export const isExercise = (param: unknown): param is Exercise => {
+  if (!isStringRecord(param))
+    return false;
+
+  return multiCheck(
+    ["id", "points", "maxPoints", "submissions", "title", "description"],
+    [isString,isNumber,isNumber,isStringArray,isString,isString],
+    param
+    );
+};
+
+export const isPointsProp = (param: unknown): param is PointsProp => {
+  if (!isStringRecord(param))
+    return false;
+    
+  return multiCheck(["receivedPoints", "maxPoints"], [isNumber, isNumber], param);
 };
