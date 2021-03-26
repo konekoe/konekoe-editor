bfb2a8171a2ae881bf8e200f296fbac50909744a
lari alakukku <lari.alakukku@aalto.fi>
2021-03-26 11:51:30 +0200

    Added type checkers

--- src/utils/typeCheckers.ts
+++ src/utils/typeCheckers.ts
@@ -1,16 +1,20 @@
-import { PointsProp, Exercise } from "../types";
+import { PointsProp, Exercise, ServerConnectResponse } from "../types";
 
 const multiCheck = (searchValues: string[], typeCheckers: Array<(p: unknown) => boolean>, target: Record<string, unknown>): boolean => {
   return searchValues.reduce((acc: boolean, curr: string, index: number) => acc && typeCheckers[index](target[curr]), true);
 }; 
 
+const arrayReducer = (typeCheck: (p: unknown) => boolean) => (acc: boolean, curr: unknown) => acc && typeCheck(curr);
+
 export const isString = (param: unknown): param is string => typeof param === "string" || param instanceof(String);
 
+export const isArray = (param: unknown): param is [] => param instanceof Array;
+
 export const isStringArray = (param: unknown): param is string[] => {
-  if (!(param instanceof Array))
+  if (!(isArray(param)))
     return false;
 
-  return param.reduce((acc: boolean, curr: unknown) => acc && isString(curr), true);
+  return param.reduce(arrayReducer(isString), true);
 };
 
 export const isNumber = (param: unknown): param is number => !isNaN(Number(param));
@@ -33,9 +37,23 @@ export const isExercise = (param: unknown): param is Exercise => {
     );
 };
 
+export const isExerciseArray = (param: unknown): param is Exercise[] => {
+  if (!isArray(param))
+    return false;
+
+  return param.reduce(arrayReducer(isExercise), true);
+}
+
 export const isPointsProp = (param: unknown): param is PointsProp => {
   if (!isStringRecord(param))
     return false;
-    
+
   return multiCheck(["receivedPoints", "maxPoints"], [isNumber, isNumber], param);
+};
+
+export const isServerConnectResponse = (param: unknown): param is ServerConnectResponse => {
+  if (!isStringRecord(param))
+    return false;
+
+  return multiCheck(["exercises"], [isExerciseArray], param);
 };
