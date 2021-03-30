602d15f9d75c8dade314dc7de50213d08c1053dd
lari alakukku <lari.alakukku@aalto.fi>
2021-03-30 11:26:30 +0300

    Added tests for code submission and updated server_connect tests

--- src/test/WebSocketMessageHandler.test.ts
+++ src/test/WebSocketMessageHandler.test.ts
@@ -5,11 +5,14 @@ import configureStore from "redux-mock-store";
 import WebSocketMessageHandler from "../utils/WebSocketMessageHandler";
 import { Store } from "../state/store";
 import { waitFor } from "@testing-library/dom";
-import { exerciseInit } from "../state/exerciseSlice";
-import { ServerConnectRequest, Exercise } from "../types";
-import { submissionInit } from "../state/submissionsSlice";
+import { exerciseInit, updatePoints } from "../state/exerciseSlice";
+import { ServerConnectRequest, Exercise, SubmissionRequest, SubmissionResponse } from "../types";
+import { submissionInit, resolveSubmission } from "../state/submissionsSlice";
 import { push } from "../state/errorSlice";
-import { CriticalError, MessageError } from "../utils/errors";
+import { CriticalError, MessageError, MinorError } from "../utils/errors";
+
+// NOTE: The token field is always an empty string as authentication is left to the server.
+// the editor should not know how the token is used for authentication.
 
 describe("WebSocketMessageHandler", function() {
   describe("Received messages are parsed and store is updated.", function() {
@@ -36,7 +39,9 @@ describe("WebSocketMessageHandler", function() {
 
         const server = MockServer(TEST_WS_ADDRESS, {
           server_connect: (_data: ServerConnectRequest) => ({
-            exercises: [testExercise]
+            payload: {
+              exercises: [testExercise]
+            }
           })
         });
 
@@ -62,7 +67,9 @@ describe("WebSocketMessageHandler", function() {
 
         const server = MockServer(TEST_WS_ADDRESS, {
           server_connect: (_data: ServerConnectRequest) => ({
-            exercises: [testExercise]
+            payload: {
+              exercises: [testExercise]
+            }
           })
         });
 
@@ -80,9 +87,10 @@ describe("WebSocketMessageHandler", function() {
 
       it("server error produces a CriticalError", async function() {
         const server = MockServer(TEST_WS_ADDRESS, {
-          server_connect: (_data: ServerConnectRequest) => {
-            throw new MessageError("Server is busted.", "123", "Server Error");
-          }
+          server_connect: (_data: ServerConnectRequest) => ({
+            payload: { exercises: [] },
+            error: new MessageError("Server is busted.", "123", "Server Error");
+          })
         });
 
         const handler = new WebSocketMessageHandler(TEST_WS_ADDRESS, "", store);
@@ -99,16 +107,88 @@ describe("WebSocketMessageHandler", function() {
     });
 
     describe("code_submission", function() {
-      it("successful submission resolves code submission in state and updates exercise points", function() {
+      const testRequest: SubmissionRequest = {
+        exerciseId: "ex1",
+        files: []
+      };
 
+      it("successful submission resolves code submission in state and updates exercise points", async function() {
+        const testResponse: SubmissionResponse = {
+          exerciseId: "ex1",
+          points: 0,
+          maxPoints: 10
+        };
+
+        const server = MockServer(TEST_WS_ADDRESS, {
+          code_submission: (_data: SubmissionRequest) => ({
+            payload: testResponse
+          })
+        });
+
+        const handler = new WebSocketMessageHandler(TEST_WS_ADDRESS, "", store);
+
+        handler.sendMessage("code_submission", testRequest);
+
+        await waitFor(() => expect(store.dispatch).toHaveBeenCalledTimes(2));
+
+        expect(store.dispatch).toHaveBeenNthCalledWith(1, resolveSubmission(testResponse));
+        expect(store.dispatch).toHaveBeenNthCalledWith(2, updatePoints(testResponse));
+    
+        server.close();
+        server.stop();
       });
 
-      it("Incorrect payload produces a MinorError", function() {
+      it("Incorrect payload produces a MinorError", async function() {
+        const testResponse: SubmissionResponse = {
+          exerciseId: "ex1",
+          points: 0,
+        } as SubmissionResponse;
 
+        const server = MockServer(TEST_WS_ADDRESS, {
+          code_submission: (_data: SubmissionRequest) => ({
+            payload: testResponse
+          })
+        });
+
+        const handler = new WebSocketMessageHandler(TEST_WS_ADDRESS, "", store);
+
+        handler.sendMessage("code_submission", testRequest);
+
+        await waitFor(() => expect(store.dispatch).toHaveBeenCalledTimes(1));
+
+        expect(store.dispatch).toHaveBeenCalledWith(push(new MinorError("Invalid response", "MessageError")));
+
+        server.close();
+        server.stop();
       });
 
-      it("server error produces MessageError, but the submission is also resolved", function() {
+      it("server error produces MessageError, but the submission is also resolved", async function() {
         // In other words, even if the server produces an error the editor still resolves the submission request.
+
+        const testResponse: SubmissionResponse = {
+          exerciseId: "ex1",
+          points: 0,
+          maxPoints: 10
+        };
+
+        const server = MockServer(TEST_WS_ADDRESS, {
+          code_submission: (_data: SubmissionRequest) => ({
+            payload: testResponse,
+            error: new MessageError("Test", "ex1")
+          })
+        });
+
+        const handler = new WebSocketMessageHandler(TEST_WS_ADDRESS, "", store);
+
+        handler.sendMessage("code_submission", testRequest);
+
+        await waitFor(() => expect(store.dispatch).toHaveBeenCalledTimes(2));
+
+        expect(store.dispatch).toHaveBeenNthCalledWith(1, resolveSubmission(testResponse));
+        expect(store.dispatch).toHaveBeenNthCalledWith(2, updatePoints(testResponse));
+    
+        server.close();
+        server.stop();
       });
     });
 
